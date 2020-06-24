d553d27cd6a6f1f61fe087544ed5f2460fd8709f
Nathan Hill <pjsstp@gmail.com>
2020-06-23 22:14:23 -0400

    removed unused icon components from admin component page.

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -10,8 +10,6 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 import EditIcon from '@material-ui/icons/Edit';
 import ViewComfyIcon from '@material-ui/icons/ViewComfy';
